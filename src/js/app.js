1f76df59266894e1306a2532bd6dfa43bc8cacce
Gavrilov Nikolay <n.gavrilov83@gmail.com>
2021-12-16 12:27:32 +0200

    fix(utils) fix load function

--- src/js/app.js
+++ src/js/app.js
@@ -10,10 +10,22 @@ import { documentReady, pageLoad } from 'utils';
 window.jQuery = $;
 window.$ = $;
 
+// -------------------  dev widget
+if (GLOBAL_VARS.projectDevStatus) {
+	import('./dev_vendors/dev_widget').then(({default: pageWidgetIni})=> {
+		pageWidgetIni();
+	}).catch((err)=> {
+		console.log('Failed to load function')
+	})
+	console.log(process.env.NODE_ENV);
+}
+// -------------------  dev widget###
+
 const styles = ['color: #fff', 'background: #cf8e1f'].join(';');
 const message = 'Developed by Glivera-team https://glivera-team.com/';
 // eslint-disable-next-line no-console
 console.info('%c%s', styles, message);
+
 
 // -------------------  global variables
 let $body;
