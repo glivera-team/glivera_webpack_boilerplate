eea6df82638ca2a03184673b019f3afe8bdc80f0
Gavrilov Nikolay <n.gavrilov83@gmail.com>
2021-12-20 11:52:33 +0200

    fix(webpack) fix global

--- settings/webpack.prod.config.js
+++ settings/webpack.prod.config.js
@@ -2,14 +2,41 @@
 const {merge} = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpackConfiguration = require('../webpack.config');
+const environment = require('./environment');
 
 module.exports = merge(webpackConfiguration, {
 	mode: 'production',
 
 	/* Manage source maps generation process. Refer to https://webpack.js.org/configuration/devtool/#production */
 	devtool: false,
+
+	output: {
+		path: environment.paths.output,
+		filename: 'js/[name].[contenthash].bundle.js',
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.(sass|scss|css)$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 2,
+							sourceMap: false,
+							modules: false,
+						},
+					},
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
 
 	/* Optimization configuration */
 	optimization: {
@@ -23,11 +50,16 @@ module.exports = merge(webpackConfiguration, {
 	},
 
 	/* Performance treshold configuration values */
+	plugins: [
+		// Extracts CSS into separate files
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash].css',
+			chunkFilename: '[id].css',
+		}),
+	],
 	performance: {
+		hints: false,
 		maxEntrypointSize: 512000,
 		maxAssetSize: 512000,
 	},
-
-	/* Additional plugins configuration */
-	plugins: [],
 });
