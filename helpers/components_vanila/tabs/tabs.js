82ff41530884f87a301b0cac0b149094835709a8
arma <montymo87@gmail.com>
2022-02-09 21:24:07 +0200

    add tab in vanila js

---
+++ helpers/components_vanila/tabs/tabs.js
@@ -0,0 +1,24 @@
+function tabs(link, block) {
+	let linkSelector = document.querySelectorAll(link);
+	let blockSelector = document.querySelectorAll(block);
+
+	linkSelector.forEach(item => item.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let	currentData = this.getAttribute('data-tab');
+		let prefix = '[data-tab="';
+		let postfix = '"]';
+		let selector = block + prefix + currentData + postfix;
+
+		blockSelector.forEach(blockEl => blockEl.classList.remove('active_tab'));
+		linkSelector.forEach(el => el.classList.remove('active_tab'));
+
+		document.querySelector(selector).classList.add('active_tab');
+		this.classList.add('active_tab');
+	}));
+}
+
+// add in loadFunc
+// tabs('.tab_nav_item', '.tab_content');
+
+export default tabs;
