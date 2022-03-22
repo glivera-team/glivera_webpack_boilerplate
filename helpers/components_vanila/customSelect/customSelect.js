f32a6198c7bd6ee0b3a1a0d606d888c5cb7edf46
geoshavel <geoshavel@gmail.com>
2022-03-22 16:49:15 +0200

    feat(custom select): added custom select, fixed form_select mixin

---
+++ helpers/components_vanila/customSelect/customSelect.js
@@ -0,0 +1,29 @@
+import Gelect from 'gelect';
+
+function initSelects() {
+	const $selects = document.querySelectorAll('.gelect');
+
+	$selects.forEach(($select) => {
+		const newSelect = new Gelect($select, {
+			// class: string, // default 'gelect' or classList[0] of your select (if defined)
+			// placeholder: string,
+			// selected: 2,
+			// ariaMessage: string,
+			// ariaLabel: '',
+			// search: {
+			// 	placeholder: '',
+			// },
+
+			// callBacks
+			// onInit: (gelect) => {},
+			// onClick: (gelect) => {},
+			// onOpen: (gelect) => {},
+			// onClose: (gelect) => {},
+			// beforeChange: (gelect) => {},
+			// afterChange: (gelect) => {},
+			// onSearch: (event, gelect) => {},
+		});
+	});
+}
+
+export default initSelects;
