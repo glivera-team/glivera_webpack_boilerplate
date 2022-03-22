3e3d17859b51fc06117c942cb01fd30baac60ab3
geoshavel <geoshavel@gmail.com>
2022-03-22 17:05:20 +0200

    fix(custom select usage example)

--- helpers/components_vanila/customSelect/customSelect.js
+++ helpers/components_vanila/customSelect/customSelect.js
@@ -3,6 +3,8 @@ import Gelect from 'gelect';
 function initSelects() {
 	const $selects = document.querySelectorAll('.gelect');
 
+	if (!$selects.length) return;
+
 	$selects.forEach(($select) => {
 		const newSelect = new Gelect($select, {
 			// class: string, // default 'gelect' or classList[0] of your select (if defined)
