330e2901166656fe340b3e3446abb810945d2a7d
geoshavel <geoshavel@gmail.com>
2022-10-29 13:37:24 +0300

    refactor(slider snippet)

--- helpers/components/swiper/buildSwiper.js
+++ helpers/components/swiper/buildSwiper.js
@@ -1,16 +1,28 @@
-const buildSliders = (sliderClass) => {
-	let sliders = document.querySelectorAll(`${sliderClass}:not(.swiper)`);
-	if (sliders) {
-		sliders.forEach(slider => {
-			const el = slider;
-			const htmlStructure = `<div class="swiper-wrapper">${slider.innerHTML}</div>`;
-			el.classList.add('swiper');
-			el.innerHTML = htmlStructure;
-			const slides = el.querySelector('.swiper-wrapper').children;
+export const buildSwiper = (sliderNode) => {
+	if (!sliderNode) return;
 
-			Array.from(slides).forEach(slide => slide.classList.add('swiper-slide'));
-		});
-	}
+	const $slider = sliderNode;
+	const $slides = $slider.children;
+
+	if (!$slides.length) return;
+	$slider.classList.add('swiper');
+	Array.from($slides).forEach($slide => $slide.classList.add('swiper-slide'));
+
+	const htmlStructure = `<div class="swiper-wrapper">${$slider.innerHTML}</div>`;
+	$slider.innerHTML = htmlStructure;
 };
 
-export default buildSliders;
+export const removeSwiper = (sliderNode) => {
+	if (!sliderNode) return;
+
+	const $slider = sliderNode;
+
+	const $wrapper = $slider.querySelector('.swiper-wrapper');
+	const $slides = $wrapper.children;
+
+	if (!$slides.length) return;
+
+	$slider.classList.remove('swiper');
+	Array.from($slides).forEach($slide => $slide.classList.remove('swiper-slide'));
+	$slider.innerHTML = $wrapper.innerHTML;
+};
