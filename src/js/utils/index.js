afbc9c8c95a295f830846b66e8a686215707c85f
geoshavel <geoshavel@gmail.com>
2022-11-17 12:16:41 +0200

    feat(utils): detect users os func

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -97,6 +97,16 @@ export const onWindowResize = cb => {
 	handleResize();
 };
 
+export const detectUsersOS = () => {
+	if (window.navigator.userAgent.indexOf('Win') !== -1) return 'Windows OS';
+	if (window.navigator.userAgent.indexOf('Mac') !== -1) return 'Macintosh';
+	if (window.navigator.userAgent.indexOf('Linux') !== -1) return 'Linux OS';
+	if (window.navigator.userAgent.indexOf('Android') !== -1) return 'Android OS';
+	if (window.navigator.userAgent.indexOf('like Mac') !== -1) return 'iOS';
+
+	return null;
+};
+
 export const onWindowChangeOrientation = cb => {
 	if ((!cb && !isFunction(cb)) || !isTouchDevice()) return;
 
