75dbd083badaeb6e0b9a2bfbdcee2b1fa6a7a07d
Alex <alexbajbachenko2@gmail.com>
2023-02-27 15:49:48 +0200

    added to helpers: dropdown

--- helpers/components_vanila/animations/parallax/parallax.js
+++ helpers/components_vanila/animations/parallax/parallax.js
@@ -1,25 +1,32 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { exist } from '../utils/index';
 
 const parallaxInit = () => {
 	gsap.registerPlugin(ScrollTrigger);
 
 	const $parallaxItems = document.querySelectorAll('.js-parallax-item');
 
-	if (!$parallaxItems.length) return;
+	if (!exist($parallaxItems)) {
+		console.error('Parallax elements not found!');
+		return;
+	}
 
-	$parallaxItems.forEach(item => {
-		const parallaxSpeed = item.dataset.parallax_speed || '180';  // default parallax speed
-		const $trigger = item;
+	$parallaxItems.forEach(($item) => {
+		const parallaxSpeed = $item.dataset.parallaxSpeed || '180';
+		const parallaxDirection = $item.dataset.parallaxSpeed || -1;
+		const parallaxTrigger = $item.dataset.parallaxTrigger || $item;
+		const parallaxStart = $item.dataset.parallaxTrigger || 'top bottom';
+		const parallaxEnd = $item.dataset.parallaxTrigger || 'bottom top';
 
-		gsap.to(item, {
-			y: parallaxSpeed * -1,
+		gsap.to($item, {
+			y: parallaxSpeed * parallaxDirection,
 			ease: 'none',
 			scrollTrigger: {
 				id: 'parallax',
-				trigger: $trigger,
-				start: 'top 100%',
-				end: 'bottom top',
+				trigger: parallaxTrigger,
+				start: parallaxStart,
+				end: parallaxEnd,
 				ease: 'none',
 				scrub: true,
 			},
