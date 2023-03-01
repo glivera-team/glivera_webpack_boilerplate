0f18571278625eee447903ed989a7f9ff2104430
Alex <alexbajbachenko2@gmail.com>
2023-03-01 13:21:59 +0200

    refactor: (fade anim helpers) update js

--- helpers/components_vanila/animations/fade/fade-animation.js
+++ helpers/components_vanila/animations/fade/fade-animation.js
@@ -1,7 +1,30 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import { exist } from '../utils/index';
 
 gsap.registerPlugin(ScrollTrigger);
+
+const animConfigDefault = {
+	start: 'top 50%',
+	duration: 1,
+	ease: 'none',
+}
+
+const createAnimConfig = ($fadeElement) => {
+	const trigger = $fadeElement.dataset.fadeTrigger || $fadeElement;
+	const start = $fadeElement.dataset.fadeStart || animConfigDefault.start;
+	const duration = $fadeElement.dataset.fadeDuration || animConfigDefault.duration;
+	const ease = $fadeElement.dataset.fadeEase || animConfigDefault.ease;
+	const direction = $fadeElement.dataset.fadeDirection;
+	
+	return {
+		trigger,
+		start,
+		duration,
+		ease,
+		direction
+	};
+};
 
 const fadeAnim = () => {
 	const $fadeItems = document.querySelectorAll('.js-fade-item');
@@ -12,85 +35,59 @@ const fadeAnim = () => {
 	}
 
 	$fadeItems.forEach(($item) => {
-		const fadeTrigger = $item.dataset.fadeTrigger || $item;
-		const fadeStart = $item.dataset.fadeStart || 'top 50%';
-		const fadeDuration = $item.dataset.fadeDuration || 1;
-		const fadeEase = $item.dataset.fadeEase || 'none';
-		const fadeDirectionValue = $item.dataset.fadeDirection;
-
-		const fadeDirection = () => {
-			switch (fadeDirectionValue) {
-				case 'up':
-				case 'left':
-					return {
-						start: 100,
-						end: 0,
-					};
-					break;
-				case 'down':
-				case 'right':
-					return {
-						start: -100,
-						end: 0,
-					};
-					break;
+		const ANIM_CONFIG = createAnimConfig($item);
+		
+		const DIRECTIONS_MAP = new Map();
+		DIRECTIONS_MAP.set('animStartEnd', {start: 100, end: 0});
+		const animStartEnd = DIRECTIONS_MAP.get('animStartEnd');
+		
+		const setAnimDirection = (value) => {
+			return ANIM_CONFIG.direction === value;
+		};
+		const gsapSet = (axis) => {
+			return	{
+				opacity: 0,
+				...axis,
 			}
 		};
-		const direction = fadeDirection();
-
-		switch (fadeDirectionValue) {
-			case 'up':
-			case 'down':
-				gsap.set($item, {
-					opacity: 0,
-					y: direction.start,
-				});
-				break;
-			case 'right':
-			case 'left':
-				gsap.set($item, {
-					opacity: 0,
-					x: direction.start,
-				});
-				break;
-			default:
-				gsap.set($item, {
-					opacity: 0,
-				});
+		const gsapTo = (axis) => {
+			return	{
+				opacity: 1,
+				...axis,
+				duration: ANIM_CONFIG.duration,
+				ease: ANIM_CONFIG.ease,
+			}
+		};
+			
+		if (setAnimDirection('up')) {
+			gsap.set($item, gsapSet({y: animStartEnd.start}));
+		} 
+		else if (setAnimDirection('down')) {
+			gsap.set($item, gsapSet({y: -animStartEnd.start}));
+		}  
+		else if (setAnimDirection('right')) {
+			gsap.set($item, gsapSet({x: -animStartEnd.start}));
+		} 
+		else if (setAnimDirection('left')) {
+			gsap.set($item, gsapSet({x: animStartEnd.start}));
+		} 
+		else {
+			gsap.set($item, gsapSet());
 		}
 
 		ScrollTrigger.create({
 			id: 'fade',
-			trigger: fadeTrigger,
-			start: fadeStart,
+			trigger: ANIM_CONFIG.trigger,
+			start: ANIM_CONFIG.start,
 			once: true,
 			//markers: true,
 			onEnter: () => {
-				switch (fadeDirectionValue) {
-					case 'up':
-					case 'down':
-						gsap.to($item, {
-							opacity: 1,
-							y: direction.end,
-							duration: fadeDuration,
-							ease: fadeEase,
-						});
-						break;
-					case 'right':
-					case 'left':
-						gsap.to($item, {
-							opacity: 1,
-							x: direction.end,
-							duration: fadeDuration,
-							ease: fadeEase,
-						});
-						break;
-					default:
-						gsap.to($item, {
-							opacity: 1,
-							duration: fadeDuration,
-							ease: fadeEase,
-						});
+				if (setAnimDirection('up') || setAnimDirection('down')) {
+					gsap.to($item, gsapTo({y: animStartEnd.end}));
+				}  else if (setAnimDirection('right') || setAnimDirection('left')) {
+					gsap.to($item, gsapTo({x: animStartEnd.end}));
+				} else {
+					gsap.to($item, gsapTo());
 				}
 			},
 		});
@@ -103,3 +100,4 @@ export default fadeAnim;
 
 // Just add .js-fade-item class to element wich you want to use with fade.
 // If you need that the element moves during appears use 'data-fade-direction' attribute with values 'up', 'down', 'right', 'left'
+// You can change animation parameters by adding the data-attribute to animatable element. Check 'createAnimConfig' variable
