e2ed6b93587787c179b73508967a9eb93e1ecdce
Alex <alexbajbachenko2@gmail.com>
2023-03-01 13:23:51 +0200

    refactor: (paralax anim helpers) update js

--- helpers/components_vanila/animations/parallax/parallax.js
+++ helpers/components_vanila/animations/parallax/parallax.js
@@ -2,9 +2,31 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { exist } from '../utils/index';
 
-const parallaxInit = () => {
-	gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
+const animConfigDefault = {
+	speed: '180',
+	direction: -1,
+	start: 'top bottom',
+	end: 'bottom top',
+}
+const createAnimConfig = ($parallaxElement) => {
+	const trigger = $parallaxElement.dataset.parallaxTrigger || $parallaxElement;
+	const speed = $parallaxElement.dataset.parallaxSpeed || animConfigDefault.speed;
+	const direction = $parallaxElement.dataset.parallaxDirection || animConfigDefault.direction;
+	const start = $parallaxElement.dataset.parallaxTrigger || animConfigDefault.start;
+	const end = $parallaxElement.dataset.parallaxTrigger || animConfigDefault.end;
+	
+	return {
+		trigger,
+		speed,
+		direction,
+		start,
+		end,
+	};
+};
+
+const parallaxAnim = () => {
 	const $parallaxItems = document.querySelectorAll('.js-parallax-item');
 
 	if (!exist($parallaxItems)) {
@@ -13,29 +35,26 @@ const parallaxInit = () => {
 	}
 
 	$parallaxItems.forEach(($item) => {
-		const parallaxSpeed = $item.dataset.parallaxSpeed || '180';
-		const parallaxDirection = $item.dataset.parallaxSpeed || -1;
-		const parallaxTrigger = $item.dataset.parallaxTrigger || $item;
-		const parallaxStart = $item.dataset.parallaxTrigger || 'top bottom';
-		const parallaxEnd = $item.dataset.parallaxTrigger || 'bottom top';
-
+		const ANIM_CONFIG = createAnimConfig($item);
 		gsap.to($item, {
-			y: parallaxSpeed * parallaxDirection,
+			y: ANIM_CONFIG.speed * ANIM_CONFIG.direction,
 			ease: 'none',
 			scrollTrigger: {
 				id: 'parallax',
-				trigger: parallaxTrigger,
-				start: parallaxStart,
-				end: parallaxEnd,
+				trigger: ANIM_CONFIG.trigger,
+				start: ANIM_CONFIG.start,
+				end: ANIM_CONFIG.end,
 				ease: 'none',
 				scrub: true,
+				//markers: true,
 			},
 		});
 	});
 };
 
-export default parallaxInit;
+export default parallaxAnim;
 
 //  How to use
 
 // Just add .js-parallax-item class to element wich you want to be with parallax
+// You can change animation parameters by adding the data-attribute to animatable element. Check 'createAnimConfig' variable
