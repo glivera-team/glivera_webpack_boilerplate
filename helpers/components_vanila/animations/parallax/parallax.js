a54d1ef33079a667c822812bf743f863243256f7
Alex <alexbajbachenko2@gmail.com>
2023-03-03 17:52:14 +0200

    fix:(fade, parallax) fix eslint issues

--- helpers/components_vanila/animations/parallax/parallax.js
+++ helpers/components_vanila/animations/parallax/parallax.js
@@ -5,22 +5,19 @@ import { exist } from '../utils/index';
 gsap.registerPlugin(ScrollTrigger);
 
 const animConfigDefault = {
-	speed: '180',
-	direction: -1,
+	speed: '80',
 	start: 'top bottom',
-	end: 'bottom top',
-}
+	end: 'top top',
+};
 const createAnimConfig = ($parallaxElement) => {
 	const trigger = $parallaxElement.dataset.parallaxTrigger || $parallaxElement;
 	const speed = $parallaxElement.dataset.parallaxSpeed || animConfigDefault.speed;
-	const direction = $parallaxElement.dataset.parallaxDirection || animConfigDefault.direction;
 	const start = $parallaxElement.dataset.parallaxTrigger || animConfigDefault.start;
 	const end = $parallaxElement.dataset.parallaxTrigger || animConfigDefault.end;
-	
+
 	return {
 		trigger,
 		speed,
-		direction,
 		start,
 		end,
 	};
@@ -36,25 +33,29 @@ const parallaxAnim = () => {
 
 	$parallaxItems.forEach(($item) => {
 		const ANIM_CONFIG = createAnimConfig($item);
-		gsap.to($item, {
-			y: ANIM_CONFIG.speed * ANIM_CONFIG.direction,
-			ease: 'none',
-			scrollTrigger: {
-				id: 'parallax',
-				trigger: ANIM_CONFIG.trigger,
-				start: ANIM_CONFIG.start,
-				end: ANIM_CONFIG.end,
+		const setAnimConfig = (speed) => {
+			return {
+				y: speed,
 				ease: 'none',
-				scrub: true,
-				//markers: true,
-			},
-		});
+				scrollTrigger: {
+					id: 'parallax',
+					trigger: ANIM_CONFIG.trigger,
+					start: ANIM_CONFIG.start,
+					end: ANIM_CONFIG.end,
+					ease: 'none',
+					scrub: true,
+					//	markers: true,
+				},
+			};
+		};
+
+		gsap.fromTo($item, setAnimConfig(80), setAnimConfig(-80));
 	});
 };
 
 export default parallaxAnim;
 
-//  How to use
+//	How to use
 
-// Just add .js-parallax-item class to element wich you want to be with parallax
-// You can change animation parameters by adding the data-attribute to animatable element. Check 'createAnimConfig' variable
+//	Just add .js-parallax-item class to element wich you want to be with parallax
+//	You can change animation parameters by adding the data-attribute to animatable element. Check 'parallax.pug' file in this folder
