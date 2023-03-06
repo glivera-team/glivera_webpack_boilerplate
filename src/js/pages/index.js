987e898b78cda0cc4fe7c572938dc0cf500cc002
geoshavel <geoshavel@gmail.com>
2023-03-06 15:47:41 +0200

    sketch

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,9 @@
-const index = () => {};
+import Sketch from '../components/sketch';
+
+const index = () => {
+	const sketch = new Sketch({
+		dom: document.querySelector('#container'),
+	});
+};
 
 export default index;
