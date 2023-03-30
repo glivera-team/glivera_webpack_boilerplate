9fb61eaad3121d9bc1b6aea8ba08eb7d7265efeb
geoshavel <geoshavel@gmail.com>
2023-03-30 12:36:49 +0300

    fix(prettier)

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -35,7 +35,11 @@ export function debounceImmediate(delay, fn) {
 }
 
 export const isTouchDevice = () => {
-	return 'ontouchstart' in window || window.navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
+	return (
+		'ontouchstart' in window ||
+		window.navigator.maxTouchPoints > 0 ||
+		window.navigator.msMaxTouchPoints > 0
+	);
 };
 
 export const calcViewportHeight = () => {
@@ -52,7 +56,10 @@ export const calcViewportHeight = () => {
 };
 
 export const calcRemValue = ({ windowWidth, windowHeight }) => {
-	const remValue = windowWidth * 0.5625 > windowHeight ? (windowHeight / 800) * 10 : (windowWidth / 1440) * 10;
+	const remValue =
+		windowWidth * 0.5625 > windowHeight
+			? (windowHeight / 800) * 10
+			: (windowWidth / 1440) * 10;
 
 	document.documentElement.style.setProperty('--rem', `${remValue}px`);
 };
@@ -60,7 +67,10 @@ export const calcRemValue = ({ windowWidth, windowHeight }) => {
 export const calcMobileRemValue = ({ windowHeight }) => {
 	const mobileRemValue = (windowHeight / 800) * 10;
 
-	document.documentElement.style.setProperty('--mobile-rem', `${mobileRemValue}px`);
+	document.documentElement.style.setProperty(
+		'--mobile-rem',
+		`${mobileRemValue}px`,
+	);
 };
 
 export const getRandomInt = (min, max) => {
