da78c3b5ee9157f143f86fb9283aa616d6331822
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-09-26 17:01:48 +0300

    fix(swiper-update): fix note

--- helpers/components/video-viewport-controller/video-viewport-controller.js
+++ helpers/components/video-viewport-controller/video-viewport-controller.js
@@ -4,7 +4,7 @@ import ScrollTrigger from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export const videoViewportController = (videoSelector) => {
-	// NOTE: just put js-selector (class) to your video tag and specify this selector when call the fuction
+	// NOTE: just put js-selector to your video tag "NOTE: Simply add a 'js-selector' attribute to your video tag and specify this selector as an argument when calling the function
 	const $videos = document.querySelectorAll(videoSelector);
 
 	if (!$videos.length) return;
