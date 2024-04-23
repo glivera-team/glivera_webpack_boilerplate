8dd3e31d8a37fb5102a58fb28e1063e0ac5b9b41
Tanya <tetiana.tatarova@gmail.com>
2024-04-23 21:55:07 +0300

    first commit

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,3 +1,18 @@
-const index = () => {};
+const index = () => {
+	let openBtn = document.querySelector('.open-btn');
+	let closeBtn = document.querySelector('.close-btn');
+	let navbarNav = document.querySelector('.navbar_nav');
+	let navbarCollapse = document.querySelector('.navbar_collapse');
+
+	openBtn.addEventListener('click', () => {
+		navbarCollapse.classList.remove('hidden');
+	});
+	closeBtn.addEventListener('click', () => {
+		navbarCollapse.classList.add('hidden');
+	});
+	navbarNav.addEventListener('click', () => {
+		navbarCollapse.classList.add('hidden');
+	});
+};
 
 export default index;
